fbd7d653059021e300d17a81d640180155b3e411
Richardesl10 <risanchezl@unal.edu.co>
2025-11-09 10:55:43 -0500

    Update dashboardrichard.py

--- dashboardrichard.py
+++ dashboardrichard.py
@@ -775,7 +775,7 @@ Para detener el Dashboard, ejecuta la última celda de código y sigue las instr
 """
 
 #!wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-!chmod +x cloudflared-linux-amd64
+#!chmod +x cloudflared-linux-amd64
 #!mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
 
 #Ejecutar Streamlit
